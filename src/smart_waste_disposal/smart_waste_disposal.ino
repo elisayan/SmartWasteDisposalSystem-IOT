0c0fbdbbe6444a1e0e8e04ce2cf0ce926af9a41f
Elisa Yan <elisa.yan@studio.unibo.it>
2024-11-09 14:18:09 +0100

    initialized

--- src/smart_waste_disposal/smart_waste_disposal.ino
+++ src/smart_waste_disposal/smart_waste_disposal.ino
@@ -1,9 +1,12 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Assignment #2 - Smart Waste Disposal System
+ * Author: Elisa Yan
+ */
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
