2c63e58999deaca2ad30d964005673a440eec946
Elisa Yan <elisa.yan@studio.unibo.it>
2024-11-12 21:53:40 +0100

    configured pins and timeouts

---
+++ src/arduino/smart_waste_disposal/Config.h
@@ -0,0 +1,25 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define BUTTON1 4
+#define BUTTON2 5
+
+#define LED1 2
+#define LED2 3
+
+#define MOTOR 9
+
+#define PIR 6
+
+#define SONAR_TRIG 12
+#define SONAR_ECHO 13
+
+#define LM35 A0
+
+#define TIME1  5000   // Timeout for USER_DETECTED (5 seconds)
+#define TIME2  8000   // Timeout for WAITING_FOR_WASTE (8 seconds)
+#define TIME3  15000  // Duration for EMPTYING (15 seconds)
+
+#define MAX_TEMPERATURE 26
+
+#endif
