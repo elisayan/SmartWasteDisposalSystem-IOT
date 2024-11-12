54972a34b13fc62a848519a42e6dc45731dc7887
Elisa Yan <elisa.yan@studio.unibo.it>
2024-11-12 21:53:54 +0100

    updated code

--- src/arduino/smart_waste_disposal/smart_waste_disposal.ino
+++ src/arduino/smart_waste_disposal/smart_waste_disposal.ino
@@ -3,16 +3,22 @@
  * Author: Elisa Yan
  */
 
+#include "Config.h"
 #include "Servo_motor_impl.h"
 #include "LCDDisplayI2C.h"
+#include "ButtonImpl.h"
 
 ServoMotor* motor;
 LCDDisplayI2C* lcd;
+ButtonImpl* button1;
+ButtonImpl* button2;
 
 void setup() {
   Serial.begin(9600);
   motor = new ServoMotorImpl(9);
   lcd = new LCDDisplayI2C();
+  button1 = new Button(4);
+  button2 = new Button(5);
 }
 
 void loop() {
