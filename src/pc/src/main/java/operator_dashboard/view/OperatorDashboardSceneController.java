2ce525588038ab2bfbbe542b75f295c34ebee03a
Elisa Yan <elisa.yan@studio.unibo.it>
2024-11-23 18:48:02 +0100

    set up javafx scene view

---
+++ src/pc/src/main/java/operator_dashboard/view/OperatorDashboardSceneController.java
@@ -0,0 +1,36 @@
+package src.main.java.operator_dashboard.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.input.MouseEvent;
+
+public class OperatorDashboardSceneController {
+
+    @FXML
+    private Button emptyButton;
+
+    @FXML
+    private Label fillingPercentageLabel;
+
+    @FXML
+    private Button restoreButton;
+
+    @FXML
+    private Label temperatureLabel;
+
+    @FXML
+    private ProgressBar wasteProgress;
+
+    @FXML
+    void emptyClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void restoreClicked(MouseEvent event) {
+
+    }
+
+}
