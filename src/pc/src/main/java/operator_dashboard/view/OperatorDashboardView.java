58a05ae0a91bc8dd659b3e7d5555ba4960ca33b1
Elisa Yan <elisa.yan@studio.unibo.it>
2024-11-25 10:56:55 +0100

    deleted comments

--- src/pc/src/main/java/operator_dashboard/view/OperatorDashboardView.java
+++ src/pc/src/main/java/operator_dashboard/view/OperatorDashboardView.java
@@ -14,15 +14,12 @@ public class OperatorDashboardView extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(ClassLoader.getSystemResource("layouts/operator_dashboard_scene.fxml")));
-        //Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/operator_dashboard_scene.fxml"));
         Scene scene = new Scene(root);
 
         stage.setTitle("Operator Dashboard");
         stage.getIcons().add(new Image(("img/waste-bin.png")));
         stage.setScene(scene);
         stage.setResizable(false);
-        //stage.setWidth(650);
-        //stage.setHeight(500);
         stage.show();
     }
 }
